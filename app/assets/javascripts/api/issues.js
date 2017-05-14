4cd612eaad6c9521a61121e7081f9a470024b980
MATT <jestir1234@gmail.com>
2017-05-14 01:32:16 -0400

    fix

--- app/assets/javascripts/api/issues.js
+++ app/assets/javascripts/api/issues.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function(){
   // console.log("dom ready");
   //
   // const fetchTranslate = (text) => {
